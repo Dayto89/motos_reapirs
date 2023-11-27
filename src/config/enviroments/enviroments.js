fbe206720c73f7327339a956c845bded68c35a5b
Dayto89 <danielbaron297@gmail.com>
2023-11-27 09:39:28 -0500

    database

--- src/config/enviroments/enviroments.js
+++ src/config/enviroments/enviroments.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const env = require('env-var');
 
 exports.envs = {
-    PORT: env.get('PORT').required().asPortNumber()
+    PORT: env.get('PORT').required().asPortNumber(),
+    DB_URI: env.get('DB_URI').required().asString()
 }
 
